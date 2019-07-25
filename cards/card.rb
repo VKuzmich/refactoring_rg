9891b4da71c0b5dc44c8fd4d29a06f4e78a38537
VKuzmich <vjk1976@ukr.net>
2019-07-25 14:36:29 +0300

    initial commit

---
+++ cards/card.rb
@@ -0,0 +1,24 @@
+require 'yaml'
+require 'pry'
+
+class CreditCards
+  TYPES = {
+      'usual' => { balance: 50.00 },
+      'capitalist' => { balance: 100.00 },
+      'virtual' => { balance: 150.00 }
+  }.freeze
+
+  attr_accessor :type, :number, :balance
+
+  def initialize(type)
+    @type = type
+    @number = generate_number
+    @balance = TYPES.dig(type, :balance)
+  end
+
+  private
+
+  def generate_number
+    Array.new(16) { rand(10) }.join
+  end
+end
