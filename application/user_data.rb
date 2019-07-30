4157a13448fecca88378f64235f0335da3c67eb1
VKuzmich <vjk1976@ukr.net>
2019-07-30 17:51:07 +0300

    refactoring

--- application/user_data.rb
+++ application/user_data.rb
@@ -8,28 +8,24 @@ module UserData
   def user_name
     puts 'Enter your name'
     name = gets.chomp
-    name_validation(name)
     @account.name = name if valid?
   end
 
   def user_login
     puts 'Enter your login'
     login = gets.chomp
-    login_validation(login)
     @account.login = login if valid?
   end
 
   def user_password
     puts 'Enter your password'
     password = gets.chomp
-    password_validation(password)
     @account.password = password if valid?
   end
 
   def user_age
     puts 'Enter your age'
     age = gets.chomp.to_i
-    age_validation(age)
     @account.age = age if valid?
   end
 
