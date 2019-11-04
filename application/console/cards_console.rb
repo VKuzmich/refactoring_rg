c444f54e1aad430c65c960f59b9729e4e1579d68
Vladimir Kuzmich <vjk1976@ukr.net>
2019-11-04 23:13:36 +0200

    card_attributes method applied in methods where it repeats

--- application/console/cards_console.rb
+++ application/console/cards_console.rb
@@ -40,7 +40,8 @@ class CardsConsole
     return output(I18n.t('ERROR.tax_higher')) unless card.operation_put_valid?(amount)
 
     card.put_money(amount)
-    after_put_msg(amount, card)
+    output(I18n.t('common_phrases.after_put',
+                  card_attributes(amount, card, card.put_tax(amount))))
   end
 
   def validate_operation(command)
@@ -69,7 +70,8 @@ class CardsConsole
     return output(I18n.t('ERROR.no_money_left')) unless card.operation_withdraw_valid?(amount)
 
     card.withdraw_money(amount)
-    after_withdraw_msg(amount, card)
+    output(I18n.t('common_phrases.after_withdraw',
+                  card_attributes(amount, card, card.sender_tax(amount))))
   end
 
   def send_account_money(command)
@@ -97,8 +99,10 @@ class CardsConsole
   def send_money_operation(sender_card, recipient_card, amount)
     sender_card.send_money(amount)
     recipient_card.put_money(amount)
-    after_withdraw_msg(amount, sender_card)
-    after_put_msg(amount, recipient_card)
+    output(I18n.t('common_phrases.after_withdraw',
+                  card_attributes(amount, sender_card, sender_card.sender_tax(amount))))
+    output(I18n.t('common_phrases.after_put',
+                  card_attributes(amount, recipient_card, recipient_card.put_tax(amount))))
   end
 
   def recipient_card_validation
@@ -130,16 +134,6 @@ class CardsConsole
   end
 
   private
-
-  def after_withdraw_msg(amount, sender_card)
-    output(I18n.t('common_phrases.after_withdraw',
-                  card_attributes(amount, sender_card, sender_card.sender_tax(amount))))
-  end
-
-  def after_put_msg(amount, recipient_card)
-    output(I18n.t('common_phrases.after_put',
-                  card_attributes(amount, recipient_card, recipient_card.put_tax(amount))))
-  end
 
   def card_attributes(amount, card, tax)
     {amount: amount,
